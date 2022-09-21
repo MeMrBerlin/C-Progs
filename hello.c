273c3d8c420b6153f3ebb35e8c5499f0394a0962
MeMrBerlin <nishanrkm@gmail.com>
2022-09-21 19:02:29 +0530

    hello world

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// int main()
+// {
+//     printf("Hellow");
+//     printf("World");
+//     return 0;
+// }
+#include <stdio.h>
+
+int main()
+{
+    char *ptr = "Hello World";
+    printf("%s", ptr);
+
+    return 0;
+}
